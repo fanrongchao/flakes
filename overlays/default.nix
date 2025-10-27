c5ac2e0638964497e61e2072f843850447c9b3c8
Rongchao Fan <fanrongchao@gmail.com>
2025-10-27 22:02:49 +0800

    remove hello test

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,6 +1,5 @@
 final: prev: {
   #TODO: 1. add codex/claude code/opencode/openspec ... npm install -g packages
   #      2. wrap sh and npm install -g pacakges and python app(uv) make them structural with helpers
-  myHello = prev.callPackage ../pkgs/hello {};
   codex = prev.callPackage ../pkgs/codex {};
 }
