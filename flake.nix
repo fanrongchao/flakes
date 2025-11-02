707f3db8806a999e40e4726297faa2fc59370500
Rongchao Fan <f@xfa.cn>
2025-11-02 19:25:49 +0800

    add host

--- flake.nix
+++ flake.nix
@@ -98,6 +98,21 @@
         ];
       };
 
+      nixosConfigurations.rog-laptop = lib.nixosSystem {
+        inherit system;
+        modules = [
+          {nixpkgs.overlays = [(import ./overlays)];}
+          sops-nix.nixosModules.sops
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.frc = import ./users/frc/home.nix;
+          } 
+          ./hosts/rog-laptop              
+        ];
+      };
+
       #home-manager configuraions
 
       homeConfigurations.nixos = home-manager.lib.homeManagerConfiguration {
