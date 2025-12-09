d55fea626991346a54f9b4b7d36afa0a9c9e2968
Rongchao Fan <f@xfa.cn>
2025-12-09 21:26:23 +0800

    disable networkmanager

--- hosts/ai-server/configuration.nix
+++ hosts/ai-server/configuration.nix
@@ -43,7 +43,7 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
-  networking.networkmanager.enable = true;
+  networking.networkmanager.enable = false;
 
   # Set your time zone.
   time.timeZone = "Asia/Shanghai";
