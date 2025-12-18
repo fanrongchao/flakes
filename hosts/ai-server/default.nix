183ae5a1e97febba82869b31c0baaa26152350ad
Rongchao Fan <f@xfa.cn>
2025-12-18 10:04:10 +0800

    add ai profiles

--- hosts/ai-server/default.nix
+++ hosts/ai-server/default.nix
@@ -5,5 +5,7 @@
     ./configuration.nix
     #profiles
     ../../profiles/network-egress-proxy.nix
+    ../../profiles/network-ingress-proxy.nix
+    ../../profiles/zero-trust-control-plane.nix
   ];
 }
