d738c1c27404fbce9ee70f242ebc0654865fe4f8
Rongchao Fan <f@xfa.cn>
2025-12-18 10:10:52 +0800

    fix magicdns location

--- profiles/zero-trust-control-plane.nix
+++ profiles/zero-trust-control-plane.nix
@@ -4,11 +4,11 @@
     enable = true;
     settings = {
       server_url = "https://hs.zhsjf.cn";
-      listen_addr = "127.0.0.1:8080";
+      dns_config = {
+        magic_dns = true;
+        base_domain = "zhsjf.cn";   # ✅ 必填：用于 MagicDNS
+      };   listen_addr = "127.0.0.1:8080";
     };
-    dns_config = {
-      magic_dns = true;
-      base_domain = "zhsjf.cn";   # ✅ 必填：用于 MagicDNS
-    };
+   
   };
 }
