975ff6b49debfcffa9fd4de612c03a591aff44b0
Rongchao Fan <fanrongchao@gmail.com>
2025-12-21 15:29:00 +0800

    add right sha

--- profiles/network-ingress-proxy.nix
+++ profiles/network-ingress-proxy.nix
@@ -7,7 +7,7 @@
       # 你也可以换成更新的 tag（比如 v1.0.28-beta.*），但我建议先用 Latest 稳定版
       plugins = [ "github.com/caddy-dns/alidns@v1.0.26" ];
       # 第一次把 hash 留空或随便写，nixos-rebuild 会报出正确的 hash，你再填回来
-      hash = "";
+      hash = "sha256-U8uzVMPKfAMgCv1M6WIsiaLuzLbJftS87mGLeySK3FI=";
     };
 
     virtualHosts."hs.zhsjf.cn".extraConfig = ''
