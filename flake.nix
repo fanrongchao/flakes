afd435cbf5e0753862706c8b76e3ea6a181068d3
Rongchao Fan <f@xfa.cn>
2026-01-05 16:39:09 +0800

    Clean up flake hosts and users

--- flake.nix
+++ flake.nix
@@ -2,23 +2,27 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixos-wsl.url = "github:nix-community/nixos-wsl";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    dotfiles = { url = "github:fanrongchao/dotfiles"; flake = false; };
     sops-nix.url = "github:Mic92/sops-nix";
     mihomo-cli.url = "github:fanrongchao/mihomocli";
   };
 
-  outputs = { self, nixpkgs, home-manager, dotfiles, sops-nix, ... }@inputs: 
+  outputs = { self, nixpkgs, home-manager, sops-nix, ... }@inputs: 
 
     let
       system = "x86_64-linux";
+      overlays = [(import ./overlays)];
       pkgs = import nixpkgs {
-        inherit system;
-        overlays = [(import ./overlays)];
+        inherit system overlays;
+      };
+      commonNixpkgsModule = {
+        nixpkgs = {
+          inherit overlays;
+          config.allowUnfree = true;
+        };
       };
       inherit (nixpkgs) lib;
     in {
@@ -27,33 +31,10 @@
         codex = pkgs.codex;
       };
 
-      #NixOS system configurations
-      nixosConfigurations.wsl = lib.nixosSystem {
-        inherit system;
-        modules = [
-          {nixpkgs.overlays = [(import ./overlays)];}
-          sops-nix.nixosModules.sops
-          ./hosts/wsl
-          inputs.nixos-wsl.nixosModules.default
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            # 自动备份被 home-manager 管理的已存在文件
-            home-manager.backupFileExtension = "bak";
-            home-manager.extraSpecialArgs = {
-              inherit dotfiles;
-            };
-            home-manager.users.nixos = import ./users/nixos/home.nix;
-            home-manager.users.fanrongchao = import ./users/fanrongchao/home.nix;
-          }
-        ];
-      };
-
       nixosConfigurations.zenbook = lib.nixosSystem {
         inherit system;
         modules = [
-          {nixpkgs.overlays = [(import ./overlays)];}
+          commonNixpkgsModule
 	  sops-nix.nixosModules.sops
           home-manager.nixosModules.home-manager
           {
@@ -68,7 +49,7 @@
       nixosConfigurations.lg-gram = lib.nixosSystem {
         inherit system;
         modules = [
-          {nixpkgs.overlays = [(import ./overlays)];}
+          commonNixpkgsModule
 	  sops-nix.nixosModules.sops
           home-manager.nixosModules.home-manager
           {
@@ -83,7 +64,7 @@
       nixosConfigurations.pve-dev-01 = lib.nixosSystem {
         inherit system;
         modules = [
-          {nixpkgs.overlays = [(import ./overlays)];}
+          commonNixpkgsModule
 	  sops-nix.nixosModules.sops
           home-manager.nixosModules.home-manager
           {
@@ -98,7 +79,8 @@
       nixosConfigurations.ai-server= lib.nixosSystem {
         inherit system;
         modules = [
-	      sops-nix.nixosModules.sops
+          commonNixpkgsModule
+          sops-nix.nixosModules.sops
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
@@ -109,30 +91,10 @@
         ];
       };
 
-      nixosConfigurations.razer = lib.nixosSystem {
-        inherit system;
-        modules = [
-          {
-            nixpkgs = {
-              overlays = [(import ./overlays)];
-              config.allowUnfree = true; 
-            };
-          }
-          sops-nix.nixosModules.sops
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.frc = import ./users/frc/home.nix;
-          } 
-          ./hosts/razer              
-        ];
-      };
-
       nixosConfigurations.rog-laptop = lib.nixosSystem {
         inherit system;
         modules = [
-          {nixpkgs.overlays = [(import ./overlays)];}
+          commonNixpkgsModule
           sops-nix.nixosModules.sops
           home-manager.nixosModules.home-manager
           {
@@ -146,17 +108,17 @@
 
       #home-manager configuraions
 
-      homeConfigurations.nixos = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [
-          ./users/nixos/home.nix
-        ];
-      };
-
       homeConfigurations.frc = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
         modules = [
           ./users/frc/home.nix
+        ];
+      };
+
+      homeConfigurations.xfa = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [
+          ./users/xfa/home.nix
         ];
       };
     };
