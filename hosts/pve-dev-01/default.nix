2e9a0abe77d811907370af49574786319f7622aa
Rongchao Fan <f@xfa.cn>
2026-01-15 11:05:18 +0800

    Enable input-leap client on pve-dev-01

--- hosts/pve-dev-01/default.nix
+++ hosts/pve-dev-01/default.nix
@@ -6,5 +6,16 @@
     ../../profiles/workstation-ui/gnome
     ../../profiles/workstation-ui/gnome/auto-login.nix
     ../../profiles/workstation-ui/gnome/gsconnect.nix
+    ../../profiles/workstation-input.nix
   ];
+
+  # input-leap (client) -> Windows server
+  workstation.inputLeap = {
+    enable = true;
+    server = "192.168.0.150";
+    extraArgs = [ "--no-tray" "--use-x11" ];
+  };
+
+  # Avoid RemoteDesktop portal prompts; use Xorg session.
+  services.xserver.displayManager.gdm.wayland = false;
 }
