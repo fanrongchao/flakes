b562aa2225ae2c0fc1f4a9ee787f23b0e32afdde
Rongchao Fan <f@xfa.cn>
2026-01-15 10:51:51 +0800

    Add input-leap client service profile

---
+++ profiles/workstation-input.nix
@@ -0,0 +1,56 @@
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.workstation.inputLeap;
+in
+{
+  options.workstation.inputLeap = {
+    enable = lib.mkEnableOption "input-leap client (Windows host is server)";
+
+    server = lib.mkOption {
+      type = lib.types.str;
+      example = "192.168.0.10";
+      description = "input-leap server address (Windows host).";
+    };
+
+    clientName = lib.mkOption {
+      type = lib.types.str;
+      default = config.networking.hostName;
+      description = "Client screen name shown on server.";
+    };
+
+    extraArgs = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [ ];
+      description = "Extra arguments passed to input-leap client.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = [ pkgs.input-leap ];
+
+    systemd.user.services.input-leap-client = {
+      Unit = {
+        Description = "Input Leap client";
+        After = [ "graphical-session-pre.target" ];
+        PartOf = [ "graphical-session.target" ];
+      };
+
+      Service = {
+        ExecStart = lib.concatStringsSep " " ([
+          "${pkgs.input-leap}/bin/input-leapc"
+          "--name"
+          (lib.escapeShellArg cfg.clientName)
+        ] ++ (map lib.escapeShellArg cfg.extraArgs) ++ [
+          (lib.escapeShellArg cfg.server)
+        ]);
+        Restart = "on-failure";
+        RestartSec = 2;
+      };
+
+      Install = {
+        WantedBy = [ "graphical-session.target" ];
+      };
+    };
+  };
+}
