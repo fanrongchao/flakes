347911668057a47654f7ac16af50916b18457c1b
Rongchao Fan <f@xfa.cn>
2026-01-15 17:12:37 +0800

    Enable Hyprland on zenbook

--- hosts/zenbook/default.nix
+++ hosts/zenbook/default.nix
@@ -1,11 +1,16 @@
-{config, pkgs, ...}:
+{ config, pkgs, ... }:
 
 {
   imports = [
     ./configuration.nix
     #profiles
-    ../../profiles/workstation-ui/gnome
+    ../../profiles/workstation-ui/hypr
     ../../profiles/network-egress-proxy.nix
     ../../profiles/zero-trust-node.nix
+  ];
+
+  # Use Hypr ricing from ~/dotfiles on this host.
+  home-manager.users.frc.imports = [
+    ../../profiles/workstation-ui/hypr/home.nix
   ];
 }
