d9ea5568dfc52cae3db810b5d5404bfaa81bf75a
Rongchao Fan <f@xfa.cn>
2026-01-28 10:19:45 +0800

    add podman

--- hosts/lg-gram/default.nix
+++ hosts/lg-gram/default.nix
@@ -5,6 +5,7 @@
     ./configuration.nix
     #profiles
     ../../profiles/workstation-ui/dwm
+    ../../profiles/container-runtime
     ../../profiles/zero-trust-node.nix
     ../../profiles/network-egress-proxy.nix
   ];
@@ -12,4 +13,9 @@
   home-manager.users.frc.imports = [
     ./home.nix
   ];
+
+  containerRuntime = {
+    enable = true;
+    dockerCompat = true;
+  };
 }
